8874abe5acb2a8a6d0aeeb82a82c367c73c85faa
mondrian <mondrian@pc2.(none)>
2009-04-27 20:50:58 -0300

    corecoes nos fixtures

--- test/unit/cidade_test.rb
+++ test/unit/cidade_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class CidadeTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "testando validacoes cidades" do
+    c = Cidade.new
+    assert !c.save
   end
 end
