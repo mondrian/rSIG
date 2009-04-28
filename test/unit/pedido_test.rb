3d3c37f4bff32ab62460e46d23bd2425f207bdc2
Luiz Magno <magno@vaio.(none)>
2009-04-28 20:18:38 -0300

    correcao teste pedido

--- test/unit/pedido_test.rb
+++ test/unit/pedido_test.rb
@@ -11,18 +11,18 @@ class PedidoTest < ActiveSupport::TestCase
     p = Pedido.new
     p.cliente = Cliente.find(:first)
     p.vendedor = Funcionario.find(:first)
-    p.transportadora = Transportador.find(:first)
+    p.transportadora = Transportadora.find(:first)
     p.operador = Funcionario.find(:first)
     p.minuta = Minuta.find(:first)
-    p.telemarketing = Telemarketing.find(:first)
-    p.plano_pagamento = PlanoPagamento.find(:first)
+    p.telemarketing = Funcionario.find(:first)
+    p.planodepagamento = Planodepagamento.find(:first)
     p.area = Area.find(:first)
     for i in 1..10
        i = ItemPedido.new
        i.produto = Produto.find(:first)
        i.quantidade = i.valor_tabela = i.valor_venda = i.desconto = 3.5
-       p.itens << i
+       p.itemPedidos << i
     end
-    assert (p.itens.size > 0)
+    assert (p.itemPedidos.size > 0)
   end
 end
