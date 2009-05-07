52e052e25dcfb9b2630a04e80084b19c0c371a18
Luiz Magno <magno@vaio.(none)>
2009-05-07 11:00:14 -0300

    alteracao das views de clientes para uso do helper

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -13,62 +13,6 @@ class Cliente < ActiveRecord::Base
   validates_uniqueness_of :cpf_cnpj, :message => 'CNPJ/CPF duplicado'
 
 
-  def cpf_cnpj_formatado
-    if self.cpf_cnpj.size == 11 # cpf
-      self.cpf_cnpj[0,3]+'.'+self.cpf_cnpj[3,3]+'.'+self.cpf_cnpj[6,3]+'-'+self.cpf_cnpj[9,2]
-    elsif self.cpf_cnpj.size == 14 #cnpj
-      self.cpf_cnpj[0,2]+'.'+self.cpf_cnpj[2,3]+'.'+self.cpf_cnpj[5,3]+'/'+self.cpf_cnpj[8,4]+'-'+self.cpf_cnpj[12,2]
-    else
-      'Tamanho Inválido'
-    end
-  end
-
-  def fone_pessoal_formatado
-<<<<<<< HEAD:app/models/cliente.rb
-    if (!self.fone_pessoal.nil? && !self.fone_pessoal == '')
-=======
-    if (!self.fone_pessoal.nil? && self.fone_pessoal != '')
->>>>>>> ea9a1dd11673ad63a988c1f65f24d768c3c09ecb:app/models/cliente.rb
-     ( '(' + self.fone_pessoal[0,2] + ')' + self.fone_pessoal[2,4] + '-' + self.fone_pessoal[6,4])
-    end
-  end
-
-  def fone_comercial_formatado
-<<<<<<< HEAD:app/models/cliente.rb
-    if (self.fone_comercial.nil? && self.fone_comercial == '')
-=======
-    if (!self.fone_comercial.nil? && self.fone_comercial != '')
->>>>>>> ea9a1dd11673ad63a988c1f65f24d768c3c09ecb:app/models/cliente.rb
-      '(' << self.fone_comercial[0,2] << ')' << self.fone_comercial[2,4] << '-' << self.fone_comercial[6,4]
-    else
-      ''
-    end
-  end
-
-  def fone_celular_formatado
-<<<<<<< HEAD:app/models/cliente.rb
-    if (!self.fone_celular.nil? && !self.fone_celular == '')
-=======
-    if (!self.fone_celular.nil? && self.fone_celular != '')
->>>>>>> ea9a1dd11673ad63a988c1f65f24d768c3c09ecb:app/models/cliente.rb
-      '(' + self.fone_celular[0,2] + ')' + self.fone_celular[2,4] + '-' + self.fone_celular[6,4]
-    else
-      ''
-    end
-  end
-
-  def cep_formatado
-<<<<<<< HEAD:app/models/cliente.rb
-    if !(self.cep.nil? && self.cep == '')
-=======
-    if (!self.cep.nil? && self.cep != '')
->>>>>>> ea9a1dd11673ad63a988c1f65f24d768c3c09ecb:app/models/cliente.rb
-      self.cep[0,2] + '.' + self.cep[2,3] + '-' + self.cep[5,3]
-    else
-      ''
-    end
-  end
-
   private
   def remove_mascara
     self.cpf_cnpj.gsub!(/[^0-9]/,'') if !self.cpf_cnpj.nil?
